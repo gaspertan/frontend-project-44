a35bc994c87a7b0530f5c3ea924be5851c51050c
gaspertan <gasper.tan@yandex.ru>
2024-04-09 15:05:46 +0300

    9

--- bin/src/cli.js
+++ bin/src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
 function myFunction() {
-  const name = readlineSync.question('Your answer: ');
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
 
   console.log(`hello, ${name}!`);
 }
