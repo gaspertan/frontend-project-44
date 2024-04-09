5868266ca3214bdbf94a8f0b47aaba9ea99f75fb
gaspertan <gasper.tan@yandex.ru>
2024-04-09 14:35:56 +0300

    task9

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -3,13 +3,13 @@
 import readlineSync from 'readline-sync';
 
 function isPrime(num) {
-    if (num <= 1) {
+if (num <= 1) {
+    return false;
+}
+for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
         return false;
     }
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) {
-            return false;
-        }
     }
     return true;
 }
